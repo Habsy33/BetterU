fb00b9d122a4bedefc0774b3d234e8537ec6f82e
Habsy33 <holuyemo@gmail.com>
2024-02-29 15:16:21 +0000

    edit events page is mostly finished

---
+++ Better U Webapp/fullstack-login-form-master/fullstack-login-form-master/public/js/edit_event.js
@@ -0,0 +1,123 @@
+// function saveEvent() {
+//     // Get values from the input fields
+//     const title = document.querySelector('.text').value;
+//     const date = document.querySelector('.date').value;
+//     const time = document.querySelector('.time').value;
+//     const note = document.querySelector('.text.note').value;
+
+//     // Validate if the selected date and time are in the future
+//     const selectedDateTime = new Date(`${date}T${time}`);
+//     const currentDateTime = new Date();
+
+    // if (selectedDateTime <= currentDateTime) {
+    //     alert('Please select a future date and time.');
+    //     return;
+    // }
+
+//     // Create an object to represent the event
+//     const event = {
+//         title: title,
+//         date: date,
+//         time: time,
+//         note: note
+//     };
+
+//     // Log the event object to the console
+//     console.log('Saved Event:', event);
+// }
+
+
+// let isReminderSet = true; // Initial state
+
+// function toggleReminder() {
+//     // Toggle the boolean value
+//     isReminderSet = !isReminderSet;
+
+//     // Update the button color based on the boolean value
+//     const reminderButton = document.querySelector('.reminder');
+//     reminderButton.style.backgroundColor = isReminderSet ? '#42BFC6' : '#FC5F3D';
+
+//     // Log the boolean value to the console
+//     console.log('Reminder Set:', isReminderSet);
+// }
+
+// function saveEvent() {
+//     // Get values from the input fields
+//     const title = document.querySelector('.text').value;
+//     const date = document.querySelector('.date').value;
+//     const time = document.querySelector('.time').value;
+//     const note = document.querySelector('.text.note').value;
+
+//     // Create a combined date and time string
+//     const dateTimeString = `${date} ${time}`;
+
+//     // Validate if the selected date and time are in the future
+//     const selectedDateTime = new Date(dateTimeString);
+//     const currentDateTime = new Date();
+
+//     if (selectedDateTime <= currentDateTime) {
+//         alert('Please select a future date and time.');
+//         return;
+//     }
+
+//     // Create an object to represent the event
+//     const event = {
+//         title: title,
+//         date: date,
+//         time: time,
+//         note: note,
+//         setReminder: isReminderSet
+//     };
+
+//     // Log the event object to the console
+//     console.log('Saved Event:', event);
+// }
+
+let isReminderSet = true; // Default state is true
+
+function toggleReminder() {
+    // Toggle the boolean value
+    isReminderSet = !isReminderSet;
+
+    // Update the button color based on the boolean value
+    const reminderButton = document.querySelector('.reminder');
+    reminderButton.style.backgroundColor = isReminderSet ? '#42BFC6' : '#FC5F3D';
+
+    // Log the boolean value to the console
+    console.log('Reminder Set:', isReminderSet);
+}
+
+function saveEvent() {
+    // Get values from the input fields
+    const title = document.querySelector('.text').value;
+    const date = document.querySelector('.date').value;
+    const time = document.querySelector('.time').value;
+    const note = document.querySelector('.text.note').value;
+
+    // Create a combined date and time string
+    const dateTimeString = `${date} ${time}`;
+
+    // Validate if the selected date and time are in the future
+    const selectedDateTime = new Date(dateTimeString);
+    const currentDateTime = new Date();
+
+    if (selectedDateTime <= currentDateTime) {
+        alert('Please select a future date and time.');
+        return;
+    }
+
+    // Create an object to represent the event
+    const event = {
+        title: title,
+        date: date,
+        time: time,
+        note: note,
+        setReminder: isReminderSet
+    };
+
+    // Log the event object to the console
+    console.log('Saved Event:', event);
+
+    // Display an alert confirming that the event has been logged
+    alert(`Event has been logged! The Reminder has been set: ${isReminderSet}`);
+}
