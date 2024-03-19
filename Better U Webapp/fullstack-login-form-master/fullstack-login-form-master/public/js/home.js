15fa0aa0628d24e5e83f3eff9cf6c721c9b65594
Habsy33 <holuyemo@gmail.com>
2024-03-19 23:35:01 +0000

    new greetings on home page

--- Better U Webapp/fullstack-login-form-master/fullstack-login-form-master/public/js/home.js
+++ Better U Webapp/fullstack-login-form-master/fullstack-login-form-master/public/js/home.js
@@ -1,10 +1,38 @@
+// const greeting = document.querySelector('.greeting');
+
+// window.onload = () => {
+//     if(!sessionStorage.name){
+//         location.href = '/login';
+//     } else{
+//         greeting.innerHTML = `Hello, ${sessionStorage.name}!`;
+//     }
+// }
+
+// const logOut = document.querySelector('.logout');
+
+// logOut.onclick = () => {
+//     sessionStorage.clear();
+//     location.reload();
+// }
+
 const greeting = document.querySelector('.greeting');
 
 window.onload = () => {
-    if(!sessionStorage.name){
+    if (!sessionStorage.name) {
         location.href = '/login';
-    } else{
-        greeting.innerHTML = `Hello, ${sessionStorage.name}!`;
+    } else {
+        const hour = new Date().getHours();
+        let greetingMessage;
+
+        if (hour >= 5 && hour < 12) {
+            greetingMessage = 'Good morning';
+        } else if (hour >= 12 && hour < 18) {
+            greetingMessage = 'Good afternoon';
+        } else {
+            greetingMessage = 'Goodnight';
+        }
+
+        greeting.innerHTML = `${greetingMessage}, ${sessionStorage.name}!`;
     }
 }
 
